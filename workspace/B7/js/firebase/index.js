b7f8cad7bb7ab01ebd6dae221428bd69683d389a
unknown <you@example.com>
2022-01-20 16:13:32 +0700

    hw

---
+++ workspace/B7/js/firebase/index.js
@@ -0,0 +1,15 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAMITPHSZZOEdJp3VG1O8tXKsLLnt1JLN0",
+  authDomain: "ci-70-f9de1.firebaseapp.com",
+  projectId: "ci-70-f9de1",
+  storageBucket: "ci-70-f9de1.appspot.com",
+  messagingSenderId: "110858338136",
+  appId: "1:110858338136:web:ef4ab641bb6df47479af2d",
+  measurementId: "G-W026930RR0"
+};
+  
+  const appFb = firebase.initializeApp(firebaseConfig);
+  
+  const db = firebase.firestore();
+  
+  export default db;
