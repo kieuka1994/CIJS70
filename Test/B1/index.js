f338e95360b6db5006210331718d4a408562c12b
unknown <you@example.com>
2022-02-25 21:19:55 +0700

    test up class

---
+++ Test/B1/index.js
@@ -0,0 +1,32 @@
+//. 2 . Viết chương trình có đầu vào là một mảng chưa cân nặng của tất cả mọi người theo thứ tự hàng ban đầu và yêu cầu trả về mảng chưa tổng cân nặng của 2 team.
+
+  // function alternatingSums(a) {
+  // var team1 = 0;
+  // var team2 = 0;
+
+  //   return a.reduce((acc, n, i) => {
+  //     acc[i % 2] += n;
+  //     return acc;
+  //   }, [0, 0]);
+  // }
+  
+  // const nums = [60, 40, 55, 75, 64];
+  // const result = alternatingSums(nums);
+  // console.log("result: ", result); // [179, 115]
+
+// . 1 . Nhập vào một mảng các số nguyên, tìm cặp hai số liền kề có tích lớn nhất và trả về kết quả của phép nhân 2 số đó.
+
+// function adjacentElementsProduct(inputArray) {
+//     var highestNum = inputArray [0] * inputArray [1];
+
+//     for (var i = 1; i< inputArray.lenght; i++) {
+//         highestNum = Math.max(highestNum, inputArray[i] * inputArray [i-1]);
+//     }
+//     return highestNum;
+
+//     const nums = [2, 3, -5, -2, 4];
+//     const result = adjacentElementsProduct;
+//     console.log( "result: ", result ); // [10]
+    
+// }
+
